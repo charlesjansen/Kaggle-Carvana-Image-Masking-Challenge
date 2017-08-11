ba7647b14900603c6ff76c83326c266eb36facd9
charlesjansen <vondorpp@gmail.com>
2017-08-11 20:19:01 -0300

    first commit as private

---
+++ test_submit.py
@@ -0,0 +1,62 @@
+import cv2
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+from model.u_net import get_unet_128, get_unet_256, get_unet_512, get_unet_1024
+
+df_test = pd.read_csv('input/sample_submission.csv')
+ids_test = df_test['img'].map(lambda s: s.split('.')[0])
+
+input_size = 128
+batch_size = 16
+
+orig_width = 1918
+orig_height = 1280
+
+threshold = 0.5
+
+model = get_unet_128()
+model.load_weights(filepath='weights/best_weights.hdf5')
+
+names = []
+for id in ids_test:
+    names.append('{}.jpg'.format(id))
+
+
+# https://www.kaggle.com/stainsby/fast-tested-rle
+def run_length_encode(mask):
+    '''
+    img: numpy array, 1 - mask, 0 - background
+    Returns run length as string formated
+    '''
+    inds = mask.flatten()
+    runs = np.where(inds[1:] != inds[:-1])[0] + 2
+    runs[1::2] = runs[1::2] - runs[:-1:2]
+    rle = ' '.join([str(r) for r in runs])
+    return rle
+
+
+rles = []
+
+print('Predicting on {} samples with batch_size = {}...'.format(len(ids_test), batch_size))
+for start in tqdm(range(0, len(ids_test), batch_size)):
+    x_batch = []
+    end = min(start + batch_size, len(ids_test))
+    ids_test_batch = ids_test[start:end]
+    for id in ids_test_batch.values:
+        img = cv2.imread('input/test/{}.jpg'.format(id))
+        img = cv2.resize(img, (input_size, input_size))
+        x_batch.append(img)
+    x_batch = np.array(x_batch, np.float32) / 255
+    preds = model.predict_on_batch(x_batch)
+    preds = np.squeeze(preds, axis=3)
+    for pred in preds:
+        prob = cv2.resize(pred, (orig_width, orig_height))
+        mask = prob > threshold
+        rle = run_length_encode(mask)
+        rles.append(rle)
+
+print("Generating submission file...")
+df = pd.DataFrame({'img': names, 'rle_mask': rles})
+df.to_csv('submit/submission.csv.gz', index=False, compression='gzip')
